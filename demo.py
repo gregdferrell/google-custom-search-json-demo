640569071b7ff766c8b55b1f9ce7883482aa0d06
Greg Ferrell <gferrell20@gmail.com>
2017-09-22 22:24:14 -0300

    call google custom search api and display results

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+import configparser
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
@@ -24,9 +25,10 @@ form = '''<!DOCTYPE html>
 
 
 class GoogleCustomSearchDemo(http.server.BaseHTTPRequestHandler):
-
-    GOOGLE_CUSTOM_SEARCH_ENGINE_ID = '' # TODO fill in
-    GOOGLE_CUSTOM_SEARCH_API_KEY = '' # TODO fill in
+    config = configparser.ConfigParser()
+    config.read('demo.ini')
+    google_custom_search_engine_id = config['DEFAULT']['GoogleCustomSearchEngineId']
+    google_custom_search_api_key = config['DEFAULT']['GoogleCustomSearchApiKey']
 
     def do_GET(self):
         self.send_response(200)
@@ -49,32 +51,22 @@ class GoogleCustomSearchDemo(http.server.BaseHTTPRequestHandler):
 
         search_string = params["searchString"][0]
 
-        # TODO Construct URL
-        url = 'https://www.googleapis.com/customsearch/v1?q={}&cx={}&key={}'.format(search_string, self.GOOGLE_CUSTOM_SEARCH_ENGINE_ID, self.GOOGLE_CUSTOM_SEARCH_API_KEY)
-
-        # TODO Call API
-        results = (
-            {'title': 'Search Result 1', 'file_format': 'PDF/Adobe Acrobat', 'link': 'http://www.google.com',
-             'preview': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget interdum nisl, sit amet faucibus ex. Sed id sapien nec metus ultricies molestie. Nulla aliquam sodales metus sit amet vestibulum. Cras dignissim tempus magna, sagittis auctor nulla posuere vel. Phasellus justo est, cursus sit amet pretium non, laoreet in mauris.'},
-            {'title': 'Search Result 2', 'file_format': 'Word', 'link': 'http://www.foogle.com',
-             'preview': 'Aenean consectetur tortor in lorem eleifend efficitur. Nunc vehicula pretium nisl ut feugiat. Phasellus egestas ligula et nibh auctor rhoncus. Proin sit amet arcu quis lectus eleifend rhoncus. Duis ut vehicula sapien. Proin justo tellus, tempor in quam vel, mattis ullamcorper sem.'},
-            {'title': 'Search Result 3', 'file_format': 'PDF/Adobe Acrobat', 'link': 'http://www.doogle.com',
-             'preview': 'Vestibulum vel lorem quis eros ullamcorper lacinia in id sapien. Donec dictum mauris hendrerit, molestie justo sed, viverra ex. In fringilla vulputate egestas.'},
-            {'title': 'Search Result 4', 'file_format': 'Word', 'link': 'http://www.droogle.com',
-             'preview': 'Nulla quis condimentum massa. Pellentesque fermentum in magna eu pharetra. Morbi eu tellus eu lorem maximus gravida. Donec neque odio, efficitur sit amet scelerisque vitae, congue a erat. Vivamus lacus eros, dignissim ac convallis ac, cursus id est. Duis non dapibus nibh, in eleifend mauris. Donec quis feugiat ligula, at condimentum justo. Curabitur ut est augue. Donec malesuada nec metus id facilisis. Aliquam sit amet mauris vulputate, iaculis turpis sit amet, mattis sem.'},
-            {'title': 'Search Result 5', 'file_format': 'Word', 'link': 'http://www.schmoogle.com',
-             'preview': 'Donec metus massa, dapibus ornare molestie ut, eleifend nec est. Fusce egestas odio feugiat, tincidunt diam non, tempor nunc. Sed a elit a arcu blandit efficitur ac non libero.'}
-        )
+        # Construct URL and call API
+        url = 'https://www.googleapis.com/customsearch/v1?q={}&cx={}&key={}'.format(search_string, self.google_custom_search_engine_id, self.google_custom_search_api_key)
+        response = requests.get(url)
+        data = response.json()
 
         # Convert Results to HTML
-        h = self.get_html_div_from_search_results(results)
-
+        num_results = data.get('searchInformation').get('formattedTotalResults')
+        search_time = data.get('searchInformation').get('formattedSearchTime')
+        h = '<p>About {} results ({} seconds)</p><hr />'.format(num_results, search_time)
+        h = h + self.get_html_div_from_search_results(data.get('items'))
         html = form.format(h)
 
-        # First, send a 200 OK response.
+        # Send a 200 OK response.
         self.send_response(200)
 
-        # Then send headers.
+        # Send headers.
         self.send_header('Content-type', 'text/html; charset=utf-8')
         self.end_headers()
 
@@ -90,10 +82,10 @@ class GoogleCustomSearchDemo(http.server.BaseHTTPRequestHandler):
         for result in results:
             h = h + '<br>'
             h = h + title.format(result.get('link'), result.get('title'))
-            if 'file_format' in result:
-                h = h + file_format.format(result.get('file_format'))
-            h = h + preview.format(result.get('preview'))
-            h = h + link.format(result.get('link'))
+            if 'fileFormat' in result:
+                h = h + file_format.format(result.get('fileFormat'))
+            h = h + preview.format(result.get('htmlSnippet'))
+            h = h + link.format(result.get('htmlFormattedUrl'))
         h = h + '</div>'
         return h
 
