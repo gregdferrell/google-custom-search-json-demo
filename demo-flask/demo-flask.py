7655f2c88df544234c17a3d0742f1510bc190a45
Greg Ferrell <gferrell20@gmail.com>
2017-09-25 23:42:22 -0300

    implemented flask version of google custom search

--- demo-flask/demo-flask.py
+++ demo-flask/demo-flask.py
@@ -1,11 +1,38 @@
-from flask import Flask, render_template
+import configparser
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
+config = configparser.ConfigParser()
+config.read('demo.ini')
+google_custom_search_engine_id = config['DEFAULT']['GoogleCustomSearchEngineId']
+google_custom_search_api_key = config['DEFAULT']['GoogleCustomSearchApiKey']
 
 
 @app.route("/", methods=['GET'])
 def home():
-    return render_template('home.html', my_string="Wheeeee!", my_list=[0, 1, 2, 3, 4, 5], title="Home")
+    # Get search request param and log it
+    search_string = request.args.get('searchString', '')
+    print(search_string)
+
+    if search_string == '':
+        return render_template('home.html', num_results=0)
+
+    # Construct URL and call API
+    url = 'https://www.googleapis.com/customsearch/v1?q={}&cx={}&key={}'.format(
+        search_string, google_custom_search_engine_id, google_custom_search_api_key)
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return render_template('home.html', num_results=0, error_message='Search returned an error.')
+
+    data = response.json()
+
+    # Render search results
+    num_results = int(data.get('searchInformation').get('formattedTotalResults'))
+    search_time = data.get('searchInformation').get('formattedSearchTime')
+    results = data.get('items')
+    return render_template('home.html', num_results=num_results, search_time=search_time, results=results)
 
 
 if __name__ == '__main__':
